5d863f05856536d0cd4f909bfcd87fe133c40e79
JamesonHuang <462430725@qq.com>
2015-03-20 17:12:36 +0800

    中序 后序 构建二叉树

--- BTree/1_4.Construct Binary Tree from Inorder and Postorder Traversal/Solution.h
+++ BTree/1_4.Construct Binary Tree from Inorder and Postorder Traversal/Solution.h
@@ -1,13 +1,12 @@
 /*************************************************************************
 	> File Name:        Solution for Reverse Words in a String
 	> Description:      
-                        （1）根据前序和中序序列构建二叉树
- 
+
+                        （1）根据中序和后序序列构建二叉树
 间
     > Conclusion:           
 
 	> Author:           rh_Jameson
-	> Created Time:     2014年12月07日 星期日 17时10分20秒
  ************************************************************************/
 #include<string>
 #include<cctype>
@@ -20,23 +19,19 @@ using namespace std;
 
 class Solution {
 public:
-/*======================先序 & 中序构建二叉树：Accepted,160ms======================*/
-    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
-        if(preorder.empty() || inorder.empty())
+/*======================后序 & 中序构建二叉树：Accepted,160ms======================*/
+    TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+        if(inorder.empty() || postorder.empty())
             return NULL;
-        int pre_idx = 0;
-        return buildTreeRecursion(preorder, inorder, pre_idx);
-    }
-    
-    TreeNode *buildTreeRecursion(vector<int> &preorder, vector<int> &inorder, int &pre_idx){
         //初始化根结点
-        int root_value = preorder[pre_idx];
+        int post_size = postorder.size();
+        int root_value = postorder[post_size - 1];
         TreeNode *root = new TreeNode(root_value);
         //只有根结点的情况
-        if(preorder.size() == 1 && preorder.size() == inorder.size() && 
-            preorder[pre_idx] == inorder[pre_idx]){
+        if(postorder.size() == 1 && postorder.size() == inorder.size() && 
+            root_value == inorder[0]){
                 return root;
-            }
+        }
         //遍历中序序列，查找根结点位置
         int iter = 0;
         while(iter < inorder.size() && inorder[iter] != root_value){
@@ -48,17 +43,19 @@ public:
         }
         //创建左子树
         vector<int> tmp_in(inorder.begin(), inorder.begin() + iter);
-        if(!tmp_in.empty() ){
-            ++pre_idx;
-            root->left = buildTreeRecursion(preorder, tmp_in, pre_idx);
+        vector<int> tmp_po(postorder.begin(), postorder.begin() + iter);
+        if(!tmp_in.empty()){
+            root->left = buildTree(tmp_in, tmp_po);
         }
-        tmp_in.clear();     //清除
         //创建右子树
-        tmp_in.assign(inorder.begin() + iter + 1, inorder.end() );
-        if(!tmp_in.empty() ){
-            pre_idx++;
-            root->right = buildTreeRecursion(preorder, tmp_in, pre_idx);
+        tmp_in.clear();
+        tmp_in.assign(inorder.begin() + iter + 1, inorder.end());
+        tmp_po.clear();
+        tmp_po.assign(postorder.begin() + iter, postorder.end() - 1);
+        if(!tmp_in.empty()){
+            root->right = buildTree(tmp_in, tmp_po);
         }
+        
         return root;
     }
 };
